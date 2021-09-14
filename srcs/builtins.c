7c17a46fefd906c747a4c617d2d3904bc5ea2241
Julie Van Audenhaege <rvan-aud@mi-r3-p5.s19.be>
2021-09-14 14:57:27 +0200

    added env command

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -8,6 +8,21 @@ int builtin_pwd()
 	if (!buff)
 		return (1);
 	buff = getcwd(buff, MAX_PATH_LEN);
+	if (!buff)
+		return (1);
 	printf("%s\n", buff);
 	free(buff);
+	return (0);
+}
+
+int	builtin_env(char **envp) //a besoin de **envp du main
+{
+	int	i;
+
+	if (!envp)
+		return (1);
+	i = 0;
+	while (envp[i])
+		printf("%s\n", envp[i++]);
+	return (0);
 }
