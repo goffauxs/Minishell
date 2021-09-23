18bbe370b1362085ee48005922cf6f08632bc31d
Robin Van audenhaege <rvan-aud@mi-r3-p5.s19.be>
2021-09-23 14:31:13 +0200

    added security in case only redirs and no cmd

--- srcs/execution/exec_pipes_forks.c
+++ srcs/execution/exec_pipes_forks.c
@@ -3,8 +3,15 @@
 void	first_child(t_script *script, char **path_env, int *pipe1)
 {
 	int ret;
+	int	nocmd;
 
-	ret = check_builtin(script->commands[0].argv[0]);
+	ret = 0;
+	nocmd = 1;
+	if (script->commands[0].argv[0])
+	{
+		ret = check_builtin(script->commands[0].argv[0]);
+		nocmd = 0;
+	}
 	if (script->commands[0].in.name)
 		in_redir(script, 0);
 	if (script->commands[0].out.name)
@@ -14,24 +21,32 @@ void	first_child(t_script *script, char **path_env, int *pipe1)
 		pipe_dup(pipe1, 1, STDOUT_FILENO);
 		close_pipes(pipe1, NULL);
 	}
-	if (!ret)
+	if (!ret && !nocmd)
 	{
 		exec_cmd(path_env, script->commands[0].argv, script->envp);
 		write(2, "command not found\n", 18);
 	}
-	else
+	else if (!nocmd)
 	{
 		handle_builtin(ret, script, 0);
 		exit(0);
 	}
+	exit(0);
 	//free etc
 }
 
 void	middle_child(t_script *script, char **path_env, int *pipein, int *pipeout, int i)
 {
 	int ret;
+	int	nocmd;
 
-	ret = check_builtin(script->commands[i].argv[0]);
+	ret = 0;
+	nocmd = 1;
+	if (script->commands[i].argv[0])
+	{
+		ret = check_builtin(script->commands[i].argv[0]);
+		nocmd = 0;
+	}
 	if (script->commands[i].in.name)
 		in_redir(script, i);
 	else
@@ -41,24 +56,32 @@ void	middle_child(t_script *script, char **path_env, int *pipein, int *pipeout, 
 	else
 		pipe_dup(pipeout, 1, STDOUT_FILENO);
 	close_pipes(pipein, pipeout);
-	if (!ret)
+	if (!ret && !nocmd)
 	{
 		exec_cmd(path_env, script->commands[i].argv, script->envp);
 		write(2, "command not found\n", 18);
 	}
-	else
+	else if (!nocmd)
 	{
 		handle_builtin(ret, script, i);
 		exit(0);
 	}
+	exit(0);
 	//free etc
 }
 
 void	last_child(t_script *script, char **path_env, int *pipein, int i)
 {
 	int ret;
+	int	nocmd;
 
-	ret = check_builtin(script->commands[i].argv[0]);
+	ret = 0;
+	nocmd = 1;
+	if (script->commands[i].argv[0])
+	{
+		ret = check_builtin(script->commands[i].argv[0]);
+		nocmd = 0;
+	}
 	if (script->commands[i].in.name)
 		in_redir(script, i);
 	else
@@ -66,15 +89,16 @@ void	last_child(t_script *script, char **path_env, int *pipein, int i)
 	if (script->commands[i].out.name)
 		out_redir(script, i);
 	close_pipes(pipein, NULL);
-	if (!ret)
+	if (!ret && !nocmd)
 	{
 		exec_cmd(path_env, script->commands[i].argv, script->envp);
 		write(2, "command not found\n", 18);
 	}
-	else
+	else if (!nocmd)
 	{
 		handle_builtin(ret, script, i);
 		exit(0);
 	}
+	exit(0);
 	//free etc
 }
